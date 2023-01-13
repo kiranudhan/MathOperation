3f37a95371b743fdd90dd0164655c5e39c7ce146
kiranudhan <udhankiran62@gmail.com>
2023-01-13 21:07:35 +0530

    UC 1.1 ->To perform math operations

--- src/math_operations/MathOperation.java
+++ src/math_operations/MathOperation.java
@@ -1,7 +1,21 @@
 package math_operations;
+@FunctionalInterface
+interface IMathFunction{
+    int calculate(int a, int b);
+}
 
 public class MathOperation {
     public static void main(String[] args) {
-        System.out.println("Welcome to math operation");
+
+        IMathFunction add = (a, b) -> a + b;
+        IMathFunction sub = (a, b) -> a - b;
+        IMathFunction multiply = (a, b) -> a * b;
+        IMathFunction divide = (a, b) -> a / b;
+
+        System.out.println("Addition is " + add.calculate(10,20));
+        System.out.println("Subtraction is " + sub.calculate(20,10));
+        System.out.println("Multiplication is " + multiply.calculate(10,20));
+        System.out.println("Division is " + divide.calculate(20,10));
+
     }
 }
