275f9906c05e0ed3115d653bda81c94c4f1e1b26
kiranudhan <udhankiran62@gmail.com>
2023-01-14 18:10:10 +0530

    passing lambda fuction to method parameter

--- src/math_operations/MathOperation.java
+++ src/math_operations/MathOperation.java
@@ -2,6 +2,9 @@ package math_operations;
 @FunctionalInterface
 interface IMathFunction{
     int calculate(int a, int b);
+    static void printResult(int a, int b, String function,IMathFunction fun){
+        System.out.println("Result of " + function + " is " + fun.calculate(a, b));
+    }
 }
 
 public class MathOperation {
@@ -17,5 +20,9 @@ public class MathOperation {
         System.out.println("Multiplication is " + multiply.calculate(10,20));
         System.out.println("Division is " + divide.calculate(20,10));
 
+        IMathFunction.printResult(6,3,"Addition", add);
+        IMathFunction.printResult(12,4,"Subtraction", sub);
+        IMathFunction.printResult(4,4,"Multiplication " , multiply);
+        IMathFunction.printResult(12,4,"Division", divide);
     }
 }
