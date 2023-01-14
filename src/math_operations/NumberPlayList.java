8b4df9f1067b80185d5d9b6e23b3272e3525a13c
kiranudhan <udhankiran62@gmail.com>
2023-01-14 19:01:42 +0530

    UC1.3->forEach method->Consumer Iterface

---
+++ src/math_operations/NumberPlayList.java
@@ -0,0 +1,50 @@
+package math_operations;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class NumberPlayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++){
+            myList.add(i);
+        }
+
+        //Method 1: using iterator
+        Iterator<Integer> itr = myList.iterator();
+        while (itr.hasNext()){
+            System.out.println("Iterator value :: " + itr.next());
+        }
+
+        //Method 2:Traversing with Consumer interface impl
+        class MyConsumer implements Consumer<Integer>{
+            @Override
+            public void accept(Integer integer) {
+                System.out.println("m2 : Consumer impl value :" + integer);
+            }
+        }
+        MyConsumer action = new MyConsumer();
+        myList.forEach(action);
+
+        //Method 3:Traversing with Anonymous Consumer interface impl
+        myList.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println("m3 : forEach anonymous class value :: " + integer );
+            }
+        });
+
+        //Method 4: Explicit Lambda Function
+        Consumer<Integer> myListAction = n -> {
+            System.out.println("m4 : forEach Lambda impl value :: " + n);
+        };
+        myList.forEach(myListAction);
+
+        //Method 5: Implicit Lambda Function
+        myList.forEach(n->{
+            System.out.println("M5 : forEach lambda impl value" + n);
+        });
+    }
+}
